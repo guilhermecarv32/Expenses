58b97af8f9d6511a6222fb6e723300898150810c
Guilherme <guilhermecm1505@gmail.com>
2026-02-11 12:09:51 -0300

    colocando teclado numerico e função no 'ok' do teclado do celular

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -9,45 +9,55 @@ class TransactionForm extends StatelessWidget {
 
   TransactionForm(this.onSubmit);
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if(title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                      labelText: 'Título',
-                    ),
-                  ),
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                      labelText: 'Valor (R\$)',
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          final title = titleController.text;
-                          final value = double.tryParse(valueController.text) ?? 0.0;
-                          onSubmit(title, value);
-                        },
-                        child: Text('Nova Transação'),
-                        style: TextButton.styleFrom(
-                          foregroundColor: Colors.purple
-                        ),
-                      ),
-                    ],
-                  )
-                ],
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Título',
               ),
             ),
-          );
+            TextField(
+              controller: valueController,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(
+                labelText: 'Valor (R\$)',
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: _submitForm,
+                  child: Text('Nova Transação'),
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.purple
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
